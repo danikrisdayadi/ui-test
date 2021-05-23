ca41b970e5a6a49cf729be9afb8444fc9d9b239b
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-05-23 18:05:00 +0800

    Remove gracia package

--- src/test/java/co/interseed/qa/ui_test/LoginPageTest.java
+++ src/test/java/co/interseed/qa/ui_test/LoginPageTest.java
@@ -10,8 +10,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class LoginPageTest {
 	WebDriver driver;
